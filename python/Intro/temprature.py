8990e27cf886f47ac13a39ae8305972d6abc7982
nadeemohc <nadeemvezhappilly@gmail.com>
2024-07-22 17:13:00 +0530

    restart

--- python/Intro/temprature.py
+++ python/Intro/temprature.py
@@ -1,3 +1,5 @@
+# Q: Convert the temprature from celsius to farenheit.
+
 temp_c = int(input('Enter the temprature in celsius: '))
 temp_f = ((9/5) * temp_c)+32
 print(f'Temprature in Fahrenheit: {temp_f}')
